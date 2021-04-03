c52f3f39920b2eac118784103e52cd7a3c2c9252
Macy Rainer <mrainer@urban.org>
2021-04-02 22:06:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg';
+import 'App.css';
 
-function App() {
+import Header from 'components/Header/Header';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Header />
       </header>
     </div>
   );
